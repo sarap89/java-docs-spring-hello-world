d18f65cc114ef2797c26837fd99c0267dadaf0bb
sarap89 <113633103+sarap89@users.noreply.github.com>
2022-09-20 12:24:21 -0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,10 @@ public class DemoApplication {
 	String sayHello() {
 		return "Hello World!";
 	}
+	
+		@GetMapping(value="/test-api")
+	String sayGetHello() {
+		return "Hello World Test API!";
+	}
+
 }
